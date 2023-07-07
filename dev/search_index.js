b3b942bfb513bd176dbee1638bfea659973265ad
Documenter.jl <documenter@juliadocs.github.io>
2023-07-07 09:46:23 +0000

    build based on fe141cc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FlyingRobots","category":"page"},{"location":"#FlyingRobots","page":"Home","title":"FlyingRobots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FlyingRobots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FlyingRobots]","category":"page"}]
+}
