3ca5d03ea307640cc33ad715b2ae54fd8a31df5b
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-07-11 21:14:22 +0200

    added 1st test for trajectory generator

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using FlyingRobots
 using Test
 
+
 @testset "FlyingRobots.jl" begin
-    # Write your tests here.
-    @test greet() == "hello"
+    # # Write your tests here.
+    include("trajectory_generation.jl")
+
 end
