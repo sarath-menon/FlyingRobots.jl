e079a07109f69c9e3869f43348df459467476eb7
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-07-12 16:19:18 +0200

    deactivate test till code structure finalized

--- test/typeutilities.jl
+++ test/typeutilities.jl
@@ -16,9 +16,6 @@ end
 
 
 
-
-
-
 function run_tests()
 
     obj = SampleStruct(1.0, 2.0)
@@ -33,14 +30,14 @@ function run_tests()
         @test [obj.x, obj.y] == vec_
     end
 
-    state = Quad2DState(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
-    control_cmd = Quad2DControlCmd(7.0, 8.0)
-    diffeq_state = convert_Fr_types_to_diffeq_state(state, control_cmd)
+    # state = Quad2DState(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
+    # control_cmd = Quad2DControlCmd(7.0, 8.0)
+    # diffeq_state = convert_Fr_types_to_diffeq_state(state, control_cmd)
 
 
-    @testset "Type Utities: High level tests" begin
-        @test diffeq_state == [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
-    end
+    # @testset "Type Utities: High level tests" begin
+    #     @test diffeq_state == [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
+    # end
 
 end
 
