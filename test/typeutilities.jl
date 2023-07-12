61c329f92c18ec0e79f5d1baa1085c88d0f9fb97
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-07-12 16:16:19 +0200

    code cleanup

--- test/typeutilities.jl
+++ test/typeutilities.jl
@@ -15,22 +15,31 @@ struct SampleStructNotFloat64
 end
 
 
-obj = SampleStruct(1.0, 2.0)
 
-# convrt struct to Float64 vector
-vec_ = struct_to_float64_vector(obj)
 
 
 
 function run_tests()
 
-    @testset "Type Utlities tests" begin
+    obj = SampleStruct(1.0, 2.0)
+    # convert struct to Float64 vector
+    vec_ = struct_to_float64_vector(obj)
+
+    @testset "Type Utities: Low level tests" begin
         @test checkif_struct_has_only_Float64(SampleStruct) == true
         @test get_struct_length(SampleStruct) == 2
 
         # check if struct elements and vector values match 
         @test [obj.x, obj.y] == vec_
+    end
 
+    state = Quad2DState(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
+    control_cmd = Quad2DControlCmd(7.0, 8.0)
+    diffeq_state = convert_Fr_types_to_diffeq_state(state, control_cmd)
+
+
+    @testset "Type Utities: High level tests" begin
+        @test diffeq_state == [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
     end
 
 end
