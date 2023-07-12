cdf4bd2ffb91b058389e0c6bb937a41e5c9f0eed
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-07-12 16:18:38 +0200

    added subtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,11 @@ using Test
 
 include("trajectory_generation.jl")
 include("dynamics.jl")
+include("typeutilities.jl")
 
 let
     @testset "FlyingRobots.jl" begin
+        TypeUtilitiesTest.run_tests()
         FlyingRobotsTest.run_tests()
         DynamicsTest.run_tests()
     end
