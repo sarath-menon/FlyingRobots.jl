e35c0a878df579caafff5e2a35ab921f30621cab
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-07-14 21:56:24 +0200

    added simulation func and tests

--- examples/quad_2d/dynamics.jl
+++ examples/quad_2d/dynamics.jl
@@ -1,172 +1,43 @@
 
-export dynamics!, dynamics_diffeq!, update_state!
-export actuator_cmd_to_ctrl_cmd
-export translational_dynamics
+export control_cb
 
-
-function dynamics!(quad_2d::Quad2D, control_cmd::Quad2DControlCmd)
-
-    # extract the parameters
-    m, L, I_xx = quad_2d.params
-
-    # extract the state
-    X = quad_2d.state
-    y = X.y
-    z = X.z
-    θ = X.θ
-    ẏ = X.ẏ
-    ż = X.ẏ
-    θ̇ = X.ẏ
-
-    a_thrust = (control_cmd.body_thrust / m) # mass normalized body thrust 
-    τ = control_cmd.body_torque
-
-    # gravity vector 
-    g_vec = @SVector [0; g] # use static array
-
-    # translation E.O.M
-    f = @SVector [0; a_thrust]
-    (ÿ, z̈) = R_2D(θ) * f + g_vec
-
-    # rotational E.O.M
-    θ̈ = τ / I_xx
-
-    return @SVector [ẏ, ż, θ̇, ÿ, z̈, θ̈]
-end
-
-function dynamics2!(state::Quad2DState, control_cmd::Quad2DControlCmd, params)
-
-    # extract the parameters
-    m, L, I_xx = params
-
-    #extract the state
-    X = state
-    y = X.y
-    z = X.z
-    θ = X.θ
-    ẏ = X.ẏ
-    ż = X.ẏ
-    θ̇ = X.ẏ
-
-    a_thrust = (control_cmd.body_thrust / m) # mass normalized body thrust 
-    f = @SVector [0; a_thrust]
-    τ = control_cmd.body_torque
-
-    # gravity vector 
-    g_vec = @SVector [0; g] # use static array
-
-    # translation E.O.M
-    (ÿ, z̈) = R_2D(θ) * f + g_vec
-
-    # rotational E.O.M
-    θ̈ = τ / I_xx
-
-    return @SVector [ẏ, ż, θ̇, ÿ, z̈, θ̈]
-
-end
-
-#Define the problem
-function dynamics4_diffeq(d_state, state, params::NamedTuple, t)
-
-    nx = 6
-
-    # extract the state
-    X = @view state[1:nx]
-
-    # extract the control input
-    U = @view state[nx+1:end]
-
-    state_ = convert(Quad2DState, X)
-    ctrl_cmd = convert(Quad2DControlCmd, U)
-
-
-    (ẏ, ż, θ̇, ÿ, z̈, θ̈) = dynamics2!(state_, ctrl_cmd, params)
-
-    d_state[1], d_state[2], d_state[3] = ẏ, ż, θ̇
-    d_state[4], d_state[5], d_state[6] = ÿ, z̈, θ̈
-    return nothing
-end
-
-# function dynamics!(quad_2d::Quad2D, X, U)
-
-#     update_state!(quad_2d, X)
-#     control_cmd = get_fr_type(U, Quad2DState)
-
-#     dynamics!(quad_2d, control_cmd)
-# end
-
-
-function update_state!(quad_2d::Quad2D, state::Vector{Float64})
-    quad_2d.state.y = state[1]
-    quad_2d.state.z = state[2]
-    quad_2d.state.θ = state[3]
-    quad_2d.state.ẏ = state[4]
-    quad_2d.state.ż = state[5]
-    quad_2d.state.θ̇ = state[6]
-end
-
-function update_state!(quad_2d::Quad2D, state::Quad2DState)
-    quad_2d.state.y = state.y
-    quad_2d.state.z = state.z
-    quad_2d.state.θ = state.θ
-    quad_2d.state.ẏ = state.ẏ
-    quad_2d.state.ż = state.ż
-    quad_2d.state.θ̇ = state.θ̇
-end
-
-
-function dynamics_diffeq!(d_diffeq_state, diffeq_state, params::NamedTuple, t)
+control_cb = PeriodicCallback(0.01, initial_affect=true, save_positions=(false, true)) do integrator
 
     # Extract the parameters
-    quad_2d = params.FrRobot
+    (; m, l, I_xx, safety_box, K) = integrator.p.quad
+    nx, nu, ny, Ts = integrator.p.frmodel
+    (; log_matrix) = integrator.p.logger
 
-    # extract the state
-    state_vec = @view diffeq_state[1:quad_2d.nx]
+    # Extract the state 
+    X = @view integrator.u[1:nx]
 
-    # extract the control input
-    control_vec = @view diffeq_state[quad_2d.nx+1:end]
+    X_req = generate_trajectory(circle_trajec, quad_obj, integrator.t)
 
-    update_state!(quad_2d, state_vec)
+    # compute control input
+    X_error = X - X_req
+    U = -K * X_error
 
-    control_cmd = Quad2DControlCmd(control_vec[1], control_vec[2])
+    # # println("X_req: $(X_req)")
+    # #println("X_error: $(X_error)")
+    #println("State: $(X)")
 
-    (ẏ, ż, θ̇, ÿ, z̈, θ̈) = dynamics!(quad_2d, control_cmd)
+    f_1_equilibirum = f_2_equilibirum = -g / 2
 
-    d_diffeq_state[1], d_diffeq_state[2], d_diffeq_state[3] = ẏ, ż, θ̇
-    d_diffeq_state[4], d_diffeq_state[5], d_diffeq_state[6] = ÿ, z̈, θ̈
+    f_1::Float64 = f_1_equilibirum + U[1]
+    f_2::Float64 = f_2_equilibirum + U[2]
 
-    return nothing
+    # constrain the control input
+    f_1 = clamp(f_1, 0.0, 12.5)
+    f_2 = clamp(f_2, 0.0, 12.5)
 
-end
+    #Update the control-signal
+    integrator.u[nx+1:end] = @SVector [f_1, f_2]
 
-function actuator_cmd_to_ctrl_cmd(quad_2d::Quad2D, actuator_cmd::Quad2DActuatorCmd)
-
-    # extract the parameters
-    m, L, I_xx = quad_2d.params
-
-    # f_left = actuator_cmd.left_motor_thrust
-    # f_right = actuator_cmd.right_motor_thrust
-
-    actuator_cmd_vec = @SVector[actuator_cmd.left_motor_thrust, actuator_cmd.right_motor_thrust]
-
-    # # compute body  thrust
-    # body_thrust = f_left + f_right
-    # mass_normalized_body_thrust = (body_thrust / m) # mass normalized body  thrust 
-
-    # # compute body thrust 
-    # body_torque = (f_left - f_right) * L
-
-    control_cmd_vec = actuator_cmd_to_ctrl_cmd_matrix(L) * actuator_cmd_vec
-    control_cmd = fr_create(Quad2DControlCmd; body_thrust=control_cmd_vec[1], body_torque=control_cmd_vec[2])
-
-    return control_cmd
-end
-
-
-
-
-
-function apply_control_cmd!(quad_2d::Quad2D, control_cmd)
+    # # logging
+    # write_row_vector!(log_matrix, integrator.u, integrator.t, Ts)
 
 end
+
+
+
 
--- test/simulation.jl
+++ test/simulation.jl
@@ -9,42 +9,80 @@ using DifferentialEquations
 
 include("./../examples/quad_2d/quad_2d.jl")
 
-quad_2d_params = (; m=1.0, L=0.1, I_xx=0.003)
-# initial_state = fr_create(Quad2DState; y=0.0, z=0.0, θ=0.0, ẏ=0.0, ż=0.0, θ̇=0.0)
-quad_2d = fr_create(Quad2D; nx=6, nu=2, state=initial_state, params=quad_2d_params)
+## Create objects 
+motor_left = fr_create(BLDCMotor; thrust_min=0.0, thrust_max=12.5)
+motor_right = fr_create(BLDCMotor; thrust_min=0.0, thrust_max=12.5)
 
-# setup callback
-control_cb = PeriodicCallback(0.01, initial_affect=true, save_positions=(false, true)) do integrator
-end
+quad_obj = fr_create(Quad2d; m=1.0, L=0.1, I_xx=0.003, motor_left, motor_right)
+frmodel_params = fr_create(FrModel; nx=6, nu=2, ny=3, Ts=0.01)
+
+safety_box = fr_create(SafetyBox; x_low=-10.0, x_high=10.0, y_low=-10.0, y_high=10.0, z_low=0.0, z_high=20.0)
+
+## Linearization
+x₀ = Pose2D(2, 1, 0, 0, 0, 0)
+
+sys_c, sys_d, AB_symbolic = FlyingRobots.linearize_system(frmodel_params.Ts, x₀, quad_obj, [-g / 2, -g / 2])
+
+##  Create an LQR controller
+Q = Diagonal([1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+R = Diagonal([1.0, 1.0])
+
+dlqr_ctrl = FlyingRobots.create_lqr_controller(Q, R; params=frmodel_params, sys=sys_d)
+
+# trajectory params
+circle_trajec = fr_create(CircleTrajectory; r=1.0, ω=0.1 * π, y₀=2.0, z₀=2.0)
+
+# vehicle parameters
+quad_params = (; m=quad_obj.m, l=quad_obj.L, I_xx=0.003, safety_box=safety_box, K=dlqr_ctrl.K)
+
+## setup simulation
+tspan = (0.0, 60.0)
+
+# logging parameters
+n_rows::Int = length(tspan[1]:frmodel_params.Ts:tspan[2])
+n_cols = 8
+log_matrix = zeros(n_rows, n_cols + 1)
+log_params = (; log_matrix=log_matrix)
+
+
+# merged params 
+params = (; quad=quad_params, trajectory=circle_trajec, frmodel=ntfromstruct(frmodel_params), logger=log_params)
+quad2d_plot = quad2d_plot_initialize(frmodel_params, tspan)
+
+#Initial Conditions
+x₀ = Pose2D(3, 1, 0, 0, 0, 0)
+initial_state = [x₀.y, x₀.z, x₀.θ, x₀.ẏ, x₀.ż, x₀.θ̇]
+u₀ = [0, 0]
+
+# initial_conditions : (initial state + intial contorl action)
+initial_conditions = vcat(initial_state, u₀)
+
+control_cb 
 
 # setup ODE
-tspan = (0.0, 10.0)
-state = zeros(8)
-prob = ODEProblem(dynamics4_diffeq, state, tspan, quad_2d_params, callback=control_cb)
+prob = ODEProblem(quad_2d_dynamics_diffeq, initial_conditions, tspan, params, callback=control_cb)
 
 # solve ODE
-@time sol = solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false, save_on=false)
+#sol = solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false, save_on=false)
 @time sol = solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false)
 
-sol.t
+# save solution to csv file
+CSV.write("logs/log_no_alloc.csv", Tables.table(log_matrix), writeheader=false)
 
-# Benchmarking 
+quad_2d_plot_normal(quad2d_plot, sol; y_ref=y_req, z_ref=z_req, theta_ref=θ_req)
 
-quad_2d_params = (; m=1.0, L=0.1, I_xx=0.003)
-initial_state = fr_create(Quad2DState; y=0.0, z=0.0, θ=0.0, ẏ=0.0, ż=0.0, θ̇=0.0)
-quad_2d = fr_create(Quad2D; nx=6, nu=2, state=initial_state, params=quad_2d_params)
-ctrl_cmd = fr_create(Quad2DControlCmd; body_thrust=-g, body_torque=0.0)
+# benchmarking 
+time_taken = @elapsed solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false, save_on=false)
 
-# core dynamics function 
-@time dynamics2!(initial_state, ctrl_cmd, quad_2d_params)
 
-state = zeros(8)
-d_state = zeros(8)
 
-##= Wrapper around core dynamics function for DifferentialEquations.jl solver =#
-@btime dynamics4_diffeq(d_state, state, quad_2d_params, 0.01)
+@testset "Type Utities: Low level tests" begin
+    
+    # Test 1: Check if no. of allocations is zero 
+    allocations = @allocated quad_2d_dynamics_diffeq(d_state, state, params, 0.1)
+    @test allocations==0
 
-##= DifferentialEquations.jl solver =#
-@btime sol = solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false, save_on=false)
-@btime sol = solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false)
+    # Test 2: Check whetther the solve time is less than  50ms
+    time_taken= @elapsed solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false, save_on=false)
+    @test time_taken<= 0.050
 end
