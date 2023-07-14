ff58aa3ddb870e42a8019b818ef3c9fd8fbeb23e
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-07-14 13:07:50 +0200

    shifted callback to  library code

--- scripts/quad_2d_test.jl
+++ scripts/quad_2d_test.jl
@@ -59,44 +59,7 @@ u₀ = [0, 0]
 initial_conditions = vcat(initial_state, u₀)
 
 # Period callback that applies discrete controller 
-control_cb = PeriodicCallback(0.01, initial_affect=true, save_positions=(false, true)) do integrator
-
-    # # Extract the parameters
-    # (; m, l, I_xx, safety_box, K) = integrator.p.quad
-    # nx, nu, ny, Ts = integrator.p.frmodel
-    # (; log_matrix) = integrator.p.logger
-
-    # # Extract the state 
-    # X = @view integrator.u[1:nx]
-
-    # X_req = generate_trajectory(circle_trajec, quad_obj, integrator.t)
-
-    # # compute control input
-    # X_error = X - X_req
-    # U = -K * X_error
-
-    # # # println("X_req: $(X_req)")
-    # # #println("X_error: $(X_error)")
-    # #println("State: $(X)")
-
-    # f_1_equilibirum = f_2_equilibirum = -g / 2
-
-    # f_1::Float64 = f_1_equilibirum + U[1]
-    # f_2::Float64 = f_2_equilibirum + U[2]
-
-    # # constrain the control input
-    # f_1 = clamp(f_1, 0.0, 12.5)
-    # f_2 = clamp(f_2, 0.0, 12.5)
-
-    # #Update the control-signal
-    # integrator.u[nx+1:end] = SA_F64[f_1, f_2]
-
-    # # logging
-    # write_row_vector!(log_matrix, integrator.u, integrator.t, Ts)
-
-end
-
-#control_cb = FlyingRobots.setup_control_cb(frmodel_params, quad_obj)
+control_cb = setup_control_cb(frmodel_params, quad_obj)
 
 # setup ODE
 prob = ODEProblem(quad_2d_dynamics_diffeq, initial_conditions, tspan, params, callback=control_cb)
--- src/dynamics/quad_2d.jl
+++ src/dynamics/quad_2d.jl
@@ -15,33 +15,31 @@ GLMakie.activate!(inline=false)
 # include("utilities.jl")
 
 function setup_control_cb(frmodel::FrModel, quad_obj::Quad2d)
-    control_cb = PeriodicCallback(frmodel.Ts, initial_affect=true, save_positions=(false, true)) do integrator
-
-        trajectory = integrator.p.trajectory
+    # Period callback that applies discrete controller 
+    control_cb = PeriodicCallback(0.01, initial_affect=true, save_positions=(false, true)) do integrator
 
         # Extract the parameters
-        (; m, l, I_xx, safety_box, controller) = integrator.p.quad
-
+        (; m, l, I_xx, safety_box, K) = integrator.p.quad
         nx, nu, ny, Ts = integrator.p.frmodel
         (; log_matrix) = integrator.p.logger
 
         # Extract the state 
         X = @view integrator.u[1:nx]
 
-        X_req = generate_trajectory(trajectory, quad_obj, integrator.t)
+        X_req = generate_trajectory(circle_trajec, quad_obj, integrator.t)
 
         # compute control input
         X_error = X - X_req
-        # U = -K * X_error
+        U = -K * X_error
 
         # # println("X_req: $(X_req)")
         # #println("X_error: $(X_error)")
         #println("State: $(X)")
 
-        # f_1::Float64 = f_1_equilibirum + U[1]
-        # f_2::Float64 = f_2_equilibirum + U[2]
+        f_1_equilibirum = f_2_equilibirum = -g / 2
 
-        (f_1, f_2) = apply_lqr_ctrl(controller, X_error)
+        f_1::Float64 = f_1_equilibirum + U[1]
+        f_2::Float64 = f_2_equilibirum + U[2]
 
         # constrain the control input
         f_1 = clamp(f_1, 0.0, 12.5)
