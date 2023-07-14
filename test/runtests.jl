7c67e157bc8cc80375230372273bc3da95bf52f3
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-07-14 08:58:34 +0200

    better structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,14 @@ using FlyingRobots
 
 using Test
 
-include("trajectory_generation.jl")
-include("dynamics.jl")
-include("controller.jl")
-include("typeutilities.jl")
-include("linearize.jl")
+
 
 let
-    @testset "FlyingRobots.jl" begin
-        TypeUtilitiesTest.run_tests()
-        FlyingRobotsTest.run_tests()
-        DynamicsTest.run_tests()
-        ControllerTest.run_tests()
+    @testset "Quad2d" begin
+        include("trajectory_generation.jl")
+        include("dynamics.jl")
+        include("controller.jl")
+        include("typeutilities.jl")
+        include("linearize.jl")
     end
 end;
