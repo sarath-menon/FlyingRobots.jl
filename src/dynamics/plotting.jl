f601863d3cf4def353fa797d899493f839e5252a
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-07-15 14:01:54 +0200

    added plot func for log matrix input

--- src/dynamics/plotting.jl
+++ src/dynamics/plotting.jl
@@ -31,7 +31,7 @@ function quad2d_plot_initialize(frmodel_params::FrModel, tspan::Tuple)
     fig = Figure(resolution=(1200, 600))
     dt = frmodel_params.Ts
 
-    range = tspan[1]:dt:(tspan[2]+dt)
+    range = tspan[1]:dt:(tspan[2])
     len::Integer = length(range)
 
     # state variables 
@@ -120,12 +120,17 @@ function quad_2d_plot_normal(plot::Quad2dPlot, log_matrix::Matrix{Float64}; y_re
     # torque = (f_1 - f_2) * 0.1
 
     # compute axis limits
-    t_axis_low = sol.t[1]
-    t_axis_high = sol.t[end]
+    t_axis_low = log_matrix[1, 1]
+    t_axis_high = log_matrix[6000, 1]
 
-    (y_low, y_high) = extrema(sol[1, :])
-    (z_low, z_high) = extrema(sol[2, :])
-    (theta_low, theta_high) = extrema(sol[3, :])
+    time_vec = @view log_matrix[:, 1]
+    y_vec = @view log_matrix[:, 2]
+    z_vec = @view log_matrix[:, 3]
+    θ_vec = @view log_matrix[:, 4]
+
+    (y_low, y_high) = extrema(y_vec)
+    (z_low, z_high) = extrema(z_vec)
+    (theta_low, theta_high) = extrema(θ_vec)
 
     # set axis limits 
     plot.y_ax.limits = (t_axis_low, t_axis_high, y_low, y_high)
@@ -133,11 +138,17 @@ function quad_2d_plot_normal(plot::Quad2dPlot, log_matrix::Matrix{Float64}; y_re
     plot.theta_ax.limits = (t_axis_low, t_axis_high, theta_low, theta_high)
 
     # set axis data
-    plot.time_vec[] = sol.t
+    plot.time_vec[] = time_vec
 
-    plot.y_vec[] = sol[1, :]
-    plot.z_vec[] = sol[2, :]
-    plot.theta_vec[] = sol[3, :]
+    plot.y_vec[] = y_vec
+    lines!(plot.y_ax, time_vec, y_ref, linestyle=:dash)
+
+    plot.z_vec[] = z_vec
+    lines!(plot.z_ax, time_vec, z_ref, linestyle=:dash)
+
+    plot.theta_vec[] = θ_vec
+    lines!(plot.theta_ax, time_vec, theta_ref, linestyle=:dash)
+
 
     # plot.thrust_vec[] = thrust
     # plot.torque_vec[] = torque
