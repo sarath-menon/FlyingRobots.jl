a297e948bc0c698bec77b73a38cee8a15619a294
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-15 13:10:15 +0200

    added read func

--- src/logger_utilities.jl
+++ src/logger_utilities.jl
@@ -8,7 +8,7 @@ struct Logger14
 
     params::Dict{Symbol,Int64}
 
-    function Logger13(; n_fields=n_fields, n_timesteps=n_timesteps)
+    function Logger14(; n_fields=n_fields, n_timesteps=n_timesteps)
         # extra column for timestep data
         log_matrix = zeros(n_timesteps, n_fields + 1)
 
@@ -31,36 +31,16 @@ function reset!(logger::Logger)
 end
 
 
-function write!(logger::Logger, row::Vector{Float64}, timestep::Float64, Ts::Float64; start_index=0)
-    # find logging index
-    n_timestep = convert(Int, round(timestep / Ts) + 1)
-
-    # index = n_timestep + start_index
-    i = logger.current_index
-
-    # fill 1st column with the timestep
-    logger.log_matrix[i, 1] = timestep
-
-    # next rows - state vector
-    for j in 1:8
-        logger.log_matrix[i, j+1] = row[j]
-    end
-
-    logger.current_index += 1
-
-    return nothing
-end
-
 
 function write!(logger::Logger, row::Vector{Float64}, timestep::Float64)
-    # index = n_timestep + start_index
+
     i = logger.params[:current_index]
 
     # fill 1st column with the timestep
     logger.log_matrix[i, 1] = timestep
 
     # next rows - state vector
-    for j in 1:8
+    for j in 1:logger.n_fields
         logger.log_matrix[i, j+1] = row[j]
     end
 
@@ -69,27 +49,6 @@ function write!(logger::Logger, row::Vector{Float64}, timestep::Float64)
     return nothing
 end
 
-
-function write!(logger::Logger, row::Vector{Float64}; Ts::Float64=0.01, timestep::Float64=0.0, initial_condition::Bool=false, offset::Int=0)
-
-    if initial_condition == true
-        n_timestep = 1
-        timestep = 0.0
-    else
-        offset = 1
-    end
-
-    # find logging index
-    n_timestep = convert(Int, round(timestep / Ts) + 1)
-    index = n_timestep + offset
-
-    A = logger.log_matrix
-
-    # fill 1st column with the timestep
-    A[index, 1] = timestep
-
-    # next rows - state vector
-    for j in 1:8
-        A[index, j+1] = row[j]
-    end
+function Base.read(logger::Logger, index::Integer)
+    return logger.log_matrix[index, 2:logger.n_fields+1]
 end
