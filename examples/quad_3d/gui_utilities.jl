710802265f640eb76635387fa415dbfdab94871e
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-23 15:00:24 +0200

    added orientation plot

--- examples/quad_3d/gui_utilities.jl
+++ examples/quad_3d/gui_utilities.jl
@@ -1,19 +1,17 @@
 
-function plot_position(state_plots, plot_data, sol)
+function plot_position(state_plots, plot_data, sol, state_indices)
+
+    i = state_indices.position[1]
+
     plot_data.time_vec[] = sol.t
 
-    plot_data.axis_1[] = sol[plant.r[1]]
-    plot_data.axis_2[] = sol[plant.r[2]]
-    plot_data.axis_3[] = sol[plant.r[3]]
+    plot_data.axis_1[] = sol[i, :]
+    plot_data.axis_2[] = sol[i+1, :]
+    plot_data.axis_3[] = sol[i+2, :]
 
-    # set axis limits 
-    # xlims!(Gui.state_plots[1], 0, sol.t[end])
-    # xlims!(Gui.state_plots[2], 0, sol.t[end])
-    # xlims!(Gui.state_plots[3], 0, sol.t[end])
-
-    autolimits!(Gui.state_plots[1])
-    autolimits!(Gui.state_plots[2])
-    autolimits!(Gui.state_plots[3])
+    autolimits!(state_plots[1])
+    autolimits!(state_plots[2])
+    autolimits!(state_plots[3])
 
     # set axis titles
     state_plots[1].title = "x position"
@@ -25,5 +23,49 @@ function plot_position(state_plots, plot_data, sol)
     state_plots[2].ylabel = "y pos [m]"
     state_plots[3].ylabel = "z pos [m]"
 end
+
+
+functip
+
+function plot_orientation(state_plots, plot_data, sol, state_indices)
+
+    plot_data.time_vec[] = sol.t
+
+    # convert quaternion attitude representation to euler angles 
+    p_vec = Float64[]
+    q_vec = Float64[]
+    r_vec = Float64[]
+
+    for i in 1:length(sol.t)
+        quat_vec = QuatRotation(sol.u[i][7:10], false)
+        r, q, p = Rotations.params(RotZYX(quat_vec))
+
+        push!(p_vec, rad2deg(p))
+        push!(q_vec, rad2deg(q))
+        push!(r_vec, rad2deg(r))
+    end
+
+    plot_data.axis_1[] = p_vec
+    plot_data.axis_2[] = q_vec
+    plot_data.axis_3[] = r_vec
+
+    autolimits!(state_plots[1])
+    autolimits!(state_plots[2])
+    autolimits!(state_plots[3])
+
+    # set axis titles
+    state_plots[1].title = "roll angle"
+    state_plots[2].title = "pitch angle"
+    state_plots[3].title = "yaw angle"
+
+    # set axis labels
+    state_plots[1].ylabel = "roll angle [°]"
+    state_plots[2].ylabel = "pitch angle [°]"
+    state_plots[3].ylabel = "yaw angle [°]"
+end
+
+
+
+
 
 
