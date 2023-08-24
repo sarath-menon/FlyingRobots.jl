c278fcd1f5925469f867d4ad24fd16d23d9a4297
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-24 21:28:15 +0200

    API update

--- examples/quad_3d/quad_3d.jl
+++ examples/quad_3d/quad_3d.jl
@@ -14,8 +14,10 @@ using YAML
 
 GLMakie.activate!(inline=false)
 
-using FlyingRobots
 using MtkLibrary
+using FlyingRobots
+
+plot_elements, plot_data = FlyingRobots.Gui.show_visualizer()
 
 include("dynamics_utilities.jl")
 include("controller_utilities.jl")
@@ -24,7 +26,8 @@ include("types.jl")
 include("dynamics.jl")
 include("controller.jl")
 include("plotting.jl")
-include("gui.jl")
+# include("gui.jl")
+
 include("gui_utilities.jl")
 include("scheduler.jl")
 include("vehicle.jl")
@@ -34,26 +37,37 @@ include("modelling.jl")
 # read settings file 
 folder_path = pwd() * "/examples/quad_3d"
 
-ctrl_yaml = load_controller_params("/parameters/controller.yml")
+
 vehicle_params = load_vehicle_params("/parameters/vehicle.yml")
+ctrl_yaml = load_controller_params("/parameters/controller.yml")
 sim_params = load_sim_params("/parameters/sim.yml", vehicle_params)
 
 include("integrator_callback.jl")
 
 # build system model
-sys = build_system_model()
+sys, subsystems = build_system_model()
 
-sol = run_sim(sys, sim_params, vehicle_params)
+## pid controllers
+x_pos_pid = PID(ctrl_yaml[:position_controller][:pid_x])
+y_pos_pid = PID(ctrl_yaml[:position_controller][:pid_y])
+z_pos_pid = PID(ctrl_yaml[:position_controller][:pid_z])
+
+roll_pid = PID(ctrl_yaml[:attitude_controller][:pid_roll])
+pitch_pid = PID(ctrl_yaml[:attitude_controller][:pid_pitch])
+
+@time sol = run_sim(sys, subsystems, sim_params, vehicle_params)
+
 
 # plotting
+
 # plot_position_attitude(sol)
 state_indices = (position=(1, 3), orientation=(7, 10), motor_thrusts=(17, 21))
 
-Gui.plot_reset(Gui.plot_data)
+FlyingRobots.Gui.plot_reset(plot_data)
 
-plot_position(Gui.state_plots, Gui.plot_data, sol, state_indices)
+plot_position(plot_elements[:state_plots], plot_data, sol, state_indices)
 # @time plot_orientation(Gui.state_plots, Gui.plot_data, sol, state_indices)
 
-plot_control_input(Gui.control_plots, Gui.plot_data, sol, state_indices, vehicle_params)
+plot_control_input(plot_elements[:control_plots], plot_data, sol, state_indices, vehicle_params)
 
 end
