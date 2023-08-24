a25c944a5673c7f814cf6b8f2c4f0367cea0cffc
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-24 15:27:05 +0200

    added documentation

--- examples/quad_3d/scheduler.jl
+++ examples/quad_3d/scheduler.jl
@@ -1,38 +1,21 @@
+
+
 function scheduler(clock, vehicle_pose, ctrl_cmd, vehicle_params)
 
-    # if clock % tasks_per_ticks[:position_controller] == 0
-    #     position_controller(vehicle_pose, ctrl_cmd, vehicle_params)
-    #     # @show clock
-    # end
+    # tasks defined in yaml file in given order 
 
-    # if clock % tasks_per_ticks[:attitude_controller] == 0
-    #     attitude_controller(vehicle_pose, ctrl_cmd, vehicle_params)
-    #     # @show clock
-    # end
-
-    for task in vehicle_params.computer.tasks
-
+        # get function from task name
         func = getfield(Main, Symbol(task.name))
 
+        # run task it it's time
         if clock % task.rate_per_tick == 0
             func(vehicle_pose, ctrl_cmd, vehicle_params, task.rate)
         end
     end
 
+    # run control allocator
     control_allocator(vehicle_pose, ctrl_cmd, vehicle_params)
 end
 
-function get_ticks_per_task(task_rates)
-
-    tasks_per_ticks = Dict()
-
-    for key in keys(task_rates)
-
-        rate = task_rates[key]
-        tasks_per_ticks[key] = Int(100 / rate)
-    end
-
-    return tasks_per_ticks
-end
 
 
