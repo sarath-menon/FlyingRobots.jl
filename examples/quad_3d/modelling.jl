f6b24079581750db706cdd9b76ac28808b90106e
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-24 20:53:27 +0200

    better variable naming

--- examples/quad_3d/modelling.jl
+++ examples/quad_3d/modelling.jl
@@ -3,22 +3,22 @@
 function build_system_model()
 
     ## initialize subsystems
-    @named plant = Quadcopter(; name=:quad1)
+    @named quadcopter = Quadcopter(; name=:quad1)
     @named controller = Controller_Zero_Order_Hold()
 
     # motor thrusts
-    eqn1 = controller.U .~ plant.f_cmd
+    eqn1 = controller.U .~ quadcopter.f_cmd
 
     # connect the subsystems
     eqns = vcat(eqn1)
     @named sys_unsimplified = ODESystem(eqns,
-        systems=[plant, controller])
+        systems=[quadcopter, controller])
 
     sys = structural_simplify(sys_unsimplified)
 
     subsystems = Dict()
 
-    subsystems[:plant] = plant
+    subsystems[:plant] = quadcopter
     subsystems[:controller] = controller
 
     return sys, subsystems
