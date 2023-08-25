2f52af67ecc89d72ac2b0cae1ce0bf5c79007fcb
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-25 23:25:36 +0200

    API update

--- examples/quad_3d/sim.jl
+++ examples/quad_3d/sim.jl
@@ -16,13 +16,13 @@ function run_sim(sys, subsystems, sim_params, vehicle_params; save=false)
 
     tspan = (0.0, sim_params.duration)
 
-    X₀ = get_initial_conditions(subsystems[:plant], vehicle_params)
-    parameters = get_parameters(subsystems[:plant], vehicle_params)
+    X₀ = get_initial_conditions(subsystems.plant, vehicle_params)
+    parameters = get_parameters(subsystems.plant, vehicle_params)
 
     reset_pid_controllers()
 
     prob = ODEProblem(sys, X₀, tspan, parameters, callback=cb)
-    sol = solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false)
+    @time sol = solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false)
 
     df = DataFrame(sol)
 
