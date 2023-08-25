3cf641abebe966b42627a064adf8ab3e5f17be7a
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-25 22:33:06 +0200

    code cleanup

--- examples/quad_3d/quad_3d.jl
+++ examples/quad_3d/quad_3d.jl
@@ -30,15 +30,12 @@ include("dynamics.jl")
 include("controller.jl")
 include("plotting.jl")
 include("logging.jl")
-# include("gui.jl")
 
-include("gui_utilities.jl")
 include("scheduler.jl")
 include("vehicle.jl")
 include("sim.jl")
 include("modelling.jl")
 
-# read settings file 
 # read settings file 
 folder_path = pwd() * "/examples/quad_3d"
 
@@ -67,47 +64,13 @@ pitch_pid = PID(ctrl_yaml[:attitude_controller][:pid_pitch])
 
 # plotting
 
-FlyingRobots.Gui.plot_reset(plot_data)
-
-plot_position(plot_elements[:state_plots], plot_data, df)
-plot_orientation(plot_elements[:state_plots], plot_data, df)
-
-plot_control_input(plot_elements[:control_plots], plot_data, df, vehicle_params)
-
-flag = FlyingRobots.Gui.plot_3d_trajectory(df, plot_elements)
-
-## new gui
-using FlyingRobots
-
 plot_elements = FlyingRobots.Gui.show_visualizer()
 
 FlyingRobots.Gui.set_sim_instance(plot_elements, df)
 
-#flag = FlyingRobots.Gui.start_3d_animation(plot_elements)
+flag = FlyingRobots.Gui.start_3d_animation(plot_elements)
 
 FlyingRobots.Gui.plot_position(plot_elements)
 FlyingRobots.Gui.plot_orientation(plot_elements)
 FlyingRobots.Gui.plot_control_input(plot_elements, motor_thrust_to_body_thrust)
-
-plot_elements[:visualizer_3d]
-## to replay log file
-# # load parameters
-# log_file_path = pwd() * "/logs/Fri,25-Aug-2023 16-01-45"
-
-# vehicle_params_path = "/parameters/vehicle.yml"
-# ctrl_yaml_path = "/parameters/controller.yml"
-# sim_params_path = "/parameters/sim.yml"
-
-# vehicle_params = load_vehicle_params(log_file_path * vehicle_params_path)
-# ctrl_yaml = load_controller_params(log_file_path * ctrl_yaml_path)
-# sim_params = load_sim_params(log_file_path * sim_params_path, vehicle_params)
-
-# # load data
-# log_sim_output = CSV.read(log_file_path * "/sim_output.csv", DataFrame)
-
-# plot_position(plot_elements[:state_plots], plot_data, log_sim_output)
-# plot_orientation(plot_elements[:state_plots], plot_data, log_sim_output)
-
-# plot_control_input(plot_elements[:control_plots], plot_data, log_sim_output, vehicle_params)
-
 end
