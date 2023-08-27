43805976f6cb0d27216b0e173e65f49299c731cd
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-27 17:45:49 +0200

    reduced allocations by almost half

--- examples/quad_3d/integrator_callback.jl
+++ examples/quad_3d/integrator_callback.jl
@@ -1,28 +1,7 @@
 
+function integrator_callback(int, sim_params=sim_params)
 
-function reference_generator(t)
-
-    r = 0.5    # circle radius 
-    ω = 0.2    # angular velocity
-
-    z_0 = 1
-
-    # circular trajectory 
-    x_ref = r * sin(ω * t)
-    y_ref = r * sin(ω * t)
-    z_ref = 1.0
-
-    # x_ref = 0.0
-    # y_ref = 0.0
-    # z_ref = 1.0
-
-    return [x_ref, y_ref, z_ref]
-end
-
-
-function integrator_callback(int)
-
-    dt = sim_params.callback_dt
+    dt::Float64 = 0.01
     g = 9.81
 
     # extract the state
@@ -36,12 +15,17 @@ function integrator_callback(int)
     q1 = quaternion_integrator(q0, ω, dt)
 
     # set attitude
-    int.u[7:10] = [q1.q.s, q1.q.v1, q1.q.v2, q1.q.v3]
+    int.u[7] = q1.q.s
+    int.u[8] = q1.q.v1
+    int.u[9] = q1.q.v2
+    int.u[10] = q1.q.v3
+
 
     # update vehicle state ------------------------------------------------
 
     # set state estimate to ground truth value for now
-    vehicle_pose = flight_controller.ram_memory[:vehicle_pose]
+    # vehicle_pose = flight_controller.ram_memory[:vehicle_pose]
+    vehicle_pose = FlyingRobots.Pose3d7()
 
     vehicle_pose.pos.x = r[1]
     vehicle_pose.pos.y = r[2]
@@ -57,25 +41,22 @@ function integrator_callback(int)
     vehicle_pose.angular_vel.y = ω[2]
     vehicle_pose.angular_vel.z = ω[3]
 
+    flight_controller.ram_memory[:vehicle_pose] = vehicle_pose
+
     # get the clock count from simulation time
-    clock_time = round(Int, int.t / dt) + 1
+    clock_time = round(Int64, int.t / dt) + 1
 
-    # get trajectory reference command
-    R = reference_generator(int.t)
-
-    # set the trajectory reference
-    trajectory_reference = flight_controller.ram_memory[:trajectory_reference]
-
-    trajectory_reference.pos.x = R[1]
-    trajectory_reference.pos.y = R[2]
-    trajectory_reference.pos.z = R[3]
-
-    # run the scheduler ------------------------------------------------
-    motor_thrusts = Computer.scheduler(flight_controller)
+    # # run the scheduler ------------------------------------------------
+    motor_thrusts = Computer.scheduler(flight_controller, int.t)
+    # motor_thrusts = Computer.scheduler(flight_controller)
 
     # set the control input
     c_index = 18
-    int.u[c_index:c_index+3] = motor_thrusts
+
+    int.u[c_index] = motor_thrusts[1]
+    int.u[c_index+1] = motor_thrusts[2]
+    int.u[c_index+2] = motor_thrusts[3]
+    int.u[c_index+3] = motor_thrusts[4]
 end
 
 
