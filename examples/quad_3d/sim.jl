2f545ff24f3ce3ba7a483dbd0281a257c9992579
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-28 12:34:42 +0200

    added stepping sim func

--- examples/quad_3d/sim.jl
+++ examples/quad_3d/sim.jl
@@ -42,3 +42,38 @@ function run_sim(sys, subsystems, sim_params, vehicle_params; save=false)
     return df
 end
 
+
+
+function run_sim_stepping1(sys, subsystems, sim_params, vehicle_params; save=false)
+
+    condition(u, t, integrator) = true
+
+    control_cb = PeriodicCallback(computer_cycle, sim_params.callback_dt, initial_affect=true, save_positions=(false, true))
+    integrator_cb = DiscreteCallback(condition, quaternion_integrator!, save_positions=(false, false))
+
+    cb_set = CallbackSet(integrator_cb, control_cb)
+
+    tspan = (0.0, sim_params.duration)
+
+    X₀ = get_initial_conditions(subsystems.plant, vehicle_params)
+    parameters = get_parameters(subsystems.plant, vehicle_params)
+
+    prob = ODEProblem(sys, X₀, tspan, parameters, callback=cb_set)
+
+    integrator = init(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false)
+
+    while floor(integrator.t) <= tspan[2]
+        step!(integrator)
+    end
+
+    df = DataFrame(integrator.sol)
+    # dataframe header formatting
+    rename!(df, replace.(names(df), r"getindex" => ""))
+    rename!(df, replace.(names(df), r"₊" => "."))
+
+    if save == true
+        log_sim(df)
+    end
+
+    return df
+end
