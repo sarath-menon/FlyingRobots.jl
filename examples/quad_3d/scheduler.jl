bc679188cdcbb46fa051051435c87144a3e5be99
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-29 11:29:19 +0200

    removed unnecessary loop

--- examples/quad_3d/scheduler.jl
+++ examples/quad_3d/scheduler.jl
@@ -2,33 +2,33 @@
 
 function scheduler(computer, t)
 
-    while true
+    # while true
 
-        #increment the main clock
-        increment_clock(computer.main_clock)
+    #increment the main clock
+    increment_clock(computer.main_clock)
 
-        # get trajectory reference command
-        R = reference_generator(t)
+    # get trajectory reference command
+    R = reference_generator(t)
 
-        # set the trajectory reference
-        trajectory_reference = computer.ram_memory[:trajectory_reference]
+    # set the trajectory reference
+    trajectory_reference = computer.ram_memory[:trajectory_reference]
 
-        trajectory_reference.pos.x = R[1]
-        trajectory_reference.pos.y = R[2]
-        trajectory_reference.pos.z = R[3]
+    trajectory_reference.pos.x = R[1]
+    trajectory_reference.pos.y = R[2]
+    trajectory_reference.pos.z = R[3]
 
-        # execute tasks defined in yaml file in given order 
-        for task in computer.tasks
-            # run task if it's time
-            if computer.main_clock.count % task.rate_per_tick == 0
-                task.func(computer, task.rate)
-            end
+    # execute tasks defined in yaml file in given order 
+    for task in computer.tasks
+        # run task if it's time
+        if computer.main_clock.count % task.rate_per_tick == 0
+            task.func(computer, task.rate)
         end
-
-        # run control allocator
-        motor_thrusts = control_allocator(computer)
-
-        return motor_thrusts
     end
+
+    # run control allocator
+    motor_thrusts = control_allocator(computer)
+
+    return motor_thrusts
+    # end
 
 end
