04cfae01f2da20a1b12cac24dc74c6348586804b
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-30 23:15:01 +0200

    code refactoring

--- examples/quad_3d/sim.jl
+++ examples/quad_3d/sim.jl
@@ -10,41 +10,38 @@ function load_sim_params(path::String, vehicle_params)
     return sim_params
 end
 
-function run_sim(sys, subsystems, sim_params, vehicle_params; save=false)
+function run_sim(sys, subsystems; save=false)
 
-    condition(u, t, integrator) = true
+    prob, tspan = sim_setup(sys, subsystems)
 
-    control_cb = PeriodicCallback(computer_cycle, sim_params.callback_dt, initial_affect=true, save_positions=(false, true))
-    integrator_cb = DiscreteCallback(condition, quaternion_integrator!, save_positions=(false, false))
-
-    cb_set = CallbackSet(integrator_cb, control_cb)
-
-    tspan = (0.0, sim_params.duration)
-
-    X₀ = get_initial_conditions(subsystems.plant, vehicle_params)
-    parameters = get_parameters(subsystems.plant, vehicle_params)
-
-    # reset_pid_controllers()
-
-    prob = ODEProblem(sys, X₀, tspan, parameters, callback=cb_set)
     @time sol = solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false)
 
-    df = DataFrame(sol)
-
-    # dataframe header formatting
-    rename!(df, replace.(names(df), r"getindex" => ""))
-    rename!(df, replace.(names(df), r"₊" => "."))
-
-    if save == true
-        log_sim(df)
-    end
+    df = sim_logging(integrator.sol)
 
     return df
 end
 
 
 
-function run_sim_stepping(sys, subsystems, sim_params, vehicle_params; save=false)
+function run_sim_stepping(sys, subsystems; save=false)
+
+    prob, tspan = sim_setup(sys, subsystems)
+
+    integrator = init(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false)
+
+    while floor(integrator.t) <= tspan[2]
+        step!(integrator)
+    end
+
+    df = sim_logging(integrator.sol)
+
+    return df
+end
+
+function sim_setup(sys, subsystems)
+    # load params
+    vehicle_params = load_vehicle_params(folder_path * vehicle_params_path)
+    sim_params = load_sim_params(folder_path * sim_params_path, vehicle_params)
 
     condition(u, t, integrator) = true
 
@@ -60,13 +57,11 @@ function run_sim_stepping(sys, subsystems, sim_params, vehicle_params; save=fals
 
     prob = ODEProblem(sys, X₀, tspan, parameters, callback=cb_set)
 
-    integrator = init(prob, Tsit5(), abstol=1e-8, reltol=1e-8, save_everystep=false)
+    return prob, tspan
+end
 
-    while floor(integrator.t) <= tspan[2]
-        step!(integrator)
-    end
-
-    df = DataFrame(integrator.sol)
+function sim_logging(sol)
+    df = DataFrame(sol)
     # dataframe header formatting
     rename!(df, replace.(names(df), r"getindex" => ""))
     rename!(df, replace.(names(df), r"₊" => "."))
