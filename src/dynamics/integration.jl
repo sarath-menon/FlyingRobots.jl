ae63d3dca7801a1a8d88e0da2b27f8b73dceb255
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-30 17:17:32 +0200

    addd in-place quaternion integration func

--- src/dynamics/integration.jl
+++ src/dynamics/integration.jl
@@ -1,3 +1,5 @@
+
+# returning function 
 function quaternion_integrator(q0, ω, dt)
     q_exp = exp(QuatRotation(0, ω[1] * dt / 2, ω[2] * dt / 2, ω[3] * dt / 2, false).q)
     q_exp_q = QuatRotation(q_exp.s, q_exp.v1, q_exp.v2, q_exp.v3, false)
@@ -6,6 +8,18 @@ function quaternion_integrator(q0, ω, dt)
 
     return q1
 end
+
+# in-place function 
+function quaternion_integrator!(q, ω, dt)
+    q_exp = exp(QuatRotation(0, ω[1] * dt / 2, ω[2] * dt / 2, ω[3] * dt / 2, false).q)
+    q_exp_q = QuatRotation(q_exp.s, q_exp.v1, q_exp.v2, q_exp.v3, false)
+
+    q = q_exp_q * q
+
+    return nothing
+end
+
+
 
 
 
