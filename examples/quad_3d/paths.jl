c0d028116392a13eda7f88c1acd4347f00a1e1dd
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-31 02:16:34 +0200

    moved path to seperate file

---
+++ examples/quad_3d/paths.jl
@@ -0,0 +1,14 @@
+
+
+# read settings file 
+folder_path = pwd() * "/examples/quad_3d"
+
+vehicle_params_path = "/parameters/vehicle.yml"
+
+sim_params_path = "/parameters/sim.yml"
+ctrl_yaml_path = "/parameters/controller.yml"
+
+# vehicle_yaml = YAML.load_file(folder_path * vehicle_params_path; dicttype=Dict{Symbol,Any})
+
+# vehicle_params = load_vehicle_params(folder_path * vehicle_params_path);
+# sim_params = load_sim_params(folder_path * sim_params_path, vehicle_params)
