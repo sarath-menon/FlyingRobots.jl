213a5fb540b86315f3ebd29590313a89d014215f
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-09-05 17:46:56 +0200

    fixed gui logic

--- src/gui/gui_setup.jl
+++ src/gui/gui_setup.jl
@@ -470,26 +470,52 @@ function define_interactions(elements, sim_time)
 
     # start stepping sim if start_sim_btn is clicked
     on(start_sim_btn.clicks) do clicks
+
         # if sim is not already running, start sim
         if sim_cmd[] == SimIdle()
 
             # start_3d_animation(elements)
             sim_cmd[] = SimRunning()
-            start_sim_btn.label = "Stop Sim"
+
             Core.println("Start sim button pressed")
+
+            # stop func reqd only for realtime sim
+            if sim_acc_mode[] == RealtimeSim()
+                start_sim_btn.label = "Stop Sim"
+            end
 
             # if sim is running, stop sim
         elseif sim_cmd[] == SimRunning()
-            sim_cmd[] = SimIdle()
-            start_sim_btn.label = "Start Sim"
-            Core.println("Stop sim button pressed")
+
+            # in relatime mode, press during sim running means stop.
+
+            if sim_acc_mode[] == RealtimeSim()
+                sim_cmd[] = SimIdle()
+
+                start_sim_btn.label = "Start Sim"
+                Core.println("Stop sim button pressed")
+
+                # In accelerated mode, it means start nex sim
+            elseif sim_acc_mode[] == AcceleratedSim()
+
+                sim_cmd[] = SimRunning()
+
+                Core.println("Starting next accelerated sim")
+            end
         end
+
+
     end
 
     sim_acc_toggle = elements[:widgets][:sim_acc_toggle]
     sim_acc_toggle_label = elements[:widgets][:sim_acc_toggle_label]
 
     on(sim_acc_toggle.active) do state
+
+        # stop current sim  and set button to "Start" if sim mode is changed 
+        start_sim_btn.label = "Start Sim"
+        sim_cmd[] = SimIdle()
+
         if state == true
             Core.println("Sim mode set to Accelerated")
             sim_acc_toggle_label.text = "Accelerated Sim"
