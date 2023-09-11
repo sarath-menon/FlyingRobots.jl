fb533620000982977a2199d6b65ed4b4a0980948
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-09-11 10:26:40 +0200

    code cleanup

--- examples/quad_3d/computer.jl
+++ examples/quad_3d/computer.jl
@@ -19,47 +19,16 @@ function create_computer(name, params_dict)
         rom_memory=rom_memory, tasks=vehicle_params.computer.tasks)
 end
 
-function reset_controllers!(computer::OnboardComputer)
-    for ctrl in computer.ram_memory[:pos_pid]
-        FlyingRobots.reset!(ctrl)
-    end
-    for ctrl in computer.ram_memory[:vel_pid]
-        FlyingRobots.reset!(ctrl)
-    end
-end
-
-function update_controller_params(computer::OnboardComputer)
-
-    # pid = computer.rom_memory.pid
-    pos_pid = computer.ram_memory[:pos_pid]
-    vel_pid = computer.ram_memory[:vel_pid]
-
-    # controller = computer.rom_memory.params[:controller]
-    ctrl_params = params_dict[:controller]
-
-    update!(pos_pid[:x], ctrl_params[:position][:pid_x])
-    update!(pos_pid[:y], ctrl_params[:position][:pid_y])
-    update!(pos_pid[:z], ctrl_params[:position][:pid_z])
-
-    update!(vel_pid[:x], ctrl_params[:velocity][:pid_x])
-    update!(vel_pid[:x], ctrl_params[:velocity][:pid_y])
-    update!(vel_pid[:x], ctrl_params[:velocity][:pid_z])
-
-end
 
 function initialize!(params_dict)
 
     vehicle_params = params_dict[:vehicle]
-
-    ctrl_params = params_dict[:controller]
 
     # joystick
     js = Joystick.connect_joystick()
 
     # ROM memory
     sensors = (; joystick=js)
-
-    # allocation_matrix = ctrl_params[:allocation_matrix]
 
     rom_memory = (; params=params_dict, sensors=sensors)
 
--- src/computer/reset.jl
+++ src/computer/reset.jl
@@ -2,13 +2,3 @@
 
 
 
-# function full_reset!(computer::OnboardComputer, reset_controllers!::Function)
-#     reset_clock!(computer.main_clock)
-#     reset_controllers!(computer)
-#     # reset_estimators(computer)
-# end
-
-# # to be defined by 
-# reset_controllers!(computer::AbstractComputer) = error("reset_controllers function has to be defined for each onboard computer")
-
-# reset_estimators!(computer::AbstractComputer) = error("reset_estimators function has to be defined for each onboard computer")
